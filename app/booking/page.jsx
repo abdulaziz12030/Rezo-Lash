ff08fdda54de4c9c1c0f1de6ced8ed7c43917734
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2026-03-29 16:06:24 +0300

    Delete app directory

--- app/booking/page.jsx
+++
@@ -1,9 +0,0 @@
-import BookingForm from "@/components/BookingForm";
-
-export const metadata = {
-  title: "Book Appointment | Rezo Lash"
-};
-
-export default function BookingPage() {
-  return <BookingForm />;
-}
