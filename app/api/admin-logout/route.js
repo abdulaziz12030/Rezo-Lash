fc3a23d601f8ea9dbe4ae6c96ec922eaabff8b26
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2026-04-24 01:53:04 +0300

    Delete app directory

--- app/api/admin-logout/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { clearAdminSession } from "@/lib/auth";
-
-export async function POST() {
-  const response = NextResponse.json({ success: true });
-  clearAdminSession(response);
-  return response;
-}
